35278e6a867cbad87c1406fb1687a1fa214ceba8
RealCorebb <33654314+RealCorebb@users.noreply.github.com>
2020-11-03 09:50:15 +0800

    Update update-components.sh

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -7,10 +7,10 @@ source ./tools/config.sh
 #
 
 if [ ! -d "$AR_COMPS/arduino" ]; then
-	git clone -b idf-release/v3.3 $AR_REPO_URL "$AR_COMPS/arduino"
+	git clone $AR_REPO_URL "$AR_COMPS/arduino"
 else
 	git -C "$AR_COMPS/arduino" fetch origin && \
-	git -C "$AR_COMPS/arduino" pull origin idf-release/v3.3
+	git -C "$AR_COMPS/arduino" pull origin master
 fi
 if [ $? -ne 0 ]; then exit 1; fi
 git -C "$AR_COMPS/arduino" submodule update --init --recursive
